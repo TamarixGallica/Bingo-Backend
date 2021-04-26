c076f84d64f90afc779da51c6b4590d53e40e75f
Jani Havukainen <github@posti.titityy.net>
2021-04-26 18:32:40 +0300

    Remove dummy API controller

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
 import path from "path";
 
-// Controllers (route handlers)
-import * as dummyController from "./controllers/dummy";
-
-// Routes
+// Route handlers
 import cardRouter from "./routes/card";
 
 // Create Express server
@@ -23,10 +20,7 @@ app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
-/**
- * API examples routes.
- */
-app.get("/dummyapi", dummyController.dummyApi);
+// Routes
 app.get("/api/card", cardRouter.getCards);
 app.get("/api/card/:id", cardRouter.getCardById);
 
--- src/controllers/dummy.ts
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import { Response, Request } from "express";
-
-export const dummyApi = (req: Request, res: Response) => {
-    res.send("Hello world!").end();
-};
